0c7ece6374791555815d3ec8152c0f73e824e955
Zee <zeeboo696@gmail.com>
2024-07-09 16:29:20 -0400

    create pantry controller

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
+
+
+// server.js
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+const User = require("./models/user.js");
+// server.js
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -19,7 +29,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +38,22 @@ app.use(
   })
 );
 
+
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
 
+// server.js
+
+app.use(passUserToView)
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/foods',foodsController);
+app.use('/users/:userId/foods',foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
