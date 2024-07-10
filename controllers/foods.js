b7569ac4f971d027cb02a7356c353a80106898cb
Zee <zeeboo696@gmail.com>
2024-07-09 22:00:00 -0400

    build the edit page and update funcitonality

--- controllers/foods.js
+++ controllers/foods.js
@@ -82,5 +82,43 @@ router.delete('/:foodId', async (req, res) => {
 });
 
 
+// controllers/applications.js
+
+router.get('/:foodId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.foods.id(req.params.foodId);
+    res.render('foods/edit.ejs', {
+      food: food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+// // controllers/applications.js`
+
+router.put('/:foodId', async (req, res) => {
+  try {
+    // Find the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the current application from the id supplied by req.params
+    const food = currentUser.foods.id(req.params.foodId);
+    // Use the Mongoose .set() method, updating the current application to reflect the new form data on `req.body`
+    food.set(req.body);
+    // Save the current user
+    await currentUser.save();
+    // Redirect back to the show view of the current application
+    res.redirect(
+      `/users/${currentUser._id}/foods/${req.params.foodId}`
+    );
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+
 
 module.exports = router;
