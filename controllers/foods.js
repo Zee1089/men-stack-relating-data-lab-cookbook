c8a70d82e8fc18511e3a8bc89ddc0be44a7939e5
Zee <zeeboo696@gmail.com>
2024-07-09 21:40:11 -0400

    Build the Show Page

--- controllers/foods.js
+++ controllers/foods.js
@@ -11,8 +11,6 @@ router.get('/', async (req, res) => {
   try {
     // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
-    // Render index.ejs, passing in all of the current user's 
-    // applications as data in the context object. 
     res.render('foods/index.ejs', {
       foods: currentUser.foods,
     });
@@ -23,11 +21,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-
-// router logic will go here - will be built later on in the lab
-// router.get('/', (req, res) => {
-//     res.render('foods/index.ejs')
-//   });
 
 
 router.get('/new', async (req, res) => {
@@ -43,11 +36,11 @@ router.post('/', async (req, res) => {
     // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
     // Push req.body (the new form data object) to the
-    // applications array of the current user
+    // foods array of the current user
     currentUser.foods.push(req.body);
     // Save changes to the user
     await currentUser.save();
-    // Redirect back to the applications index view
+    // Redirect back to the foods index view
     res.redirect(`/users/${currentUser._id}/foods`);
   } catch (error) {
     // If any errors, log them and redirect back home
@@ -56,24 +49,19 @@ router.post('/', async (req, res) => {
   }
 });
 
-
-
-//   router.get('/', async (req, res) => {
-//     try {
-//       // Look up the user from req.session
-//       const currentUser = await User.findById(req.session.user._id);
-      
-//       // Render index.ejs, passing in all of the current user's pantry items as data in the context object.
-//       res.render('foods/index.ejs', {
-//         pantry: currentUser.pantry,
-//       });
-//     } catch (error) {
-//       // If any errors, log them and redirect back home
-//       console.log(error);
-//       res.redirect('/');
-//     }
-//   });
   
-  
+router.get('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.foods.id(req.params.foodId);
+    res.render('foods/show.ejs', {
+      food: food,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
 
 module.exports = router;
