0ebbdd16900ea5867a18665b9c49acf84fb1db5f
Zee <zeeboo696@gmail.com>
2024-07-09 21:24:16 -0400

    men-stack-relating-data-lab-cookbook

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,14 +5,24 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// controllers/applications.js
+
 router.get('/', async (req, res) => {
   try {
-    res.render('foods/index.ejs');
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Render index.ejs, passing in all of the current user's 
+    // applications as data in the context object. 
+    res.render('foods/index.ejs', {
+      foods: currentUser.foods,
+    });
   } catch (error) {
+    // If any errors, log them and redirect back home
     console.log(error)
     res.redirect('/')
   }
 });
+
 
 // router logic will go here - will be built later on in the lab
 // router.get('/', (req, res) => {
